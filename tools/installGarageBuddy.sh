9752a30135044f793f37b792fd0f420c2b8f93c8
Jerry Thompson <jerrywthompson@comcast.net>
2017-08-16 15:06:38 -0500

    fixing pi_alert folder in github

--- tools/installGarageBuddy.sh
+++ tools/installGarageBuddy.sh
@@ -63,9 +63,9 @@ echo "********** Starting to update pi_garage_alert_config **********"
 
 # Add phone phone numbers for text alerting
 echo "Adding mobile phone numbers for text alerts"
-sed -i -e 's/email:someone@example.com/'"'"'email:8168853837@messaging.sprintpcs.com'"'"', '"'"'email:8165227655@tmomail.net'"'"'/g' test.py
+sed -i -e 's/email:someone@example.com/'"'"'email:8168853837@messaging.sprintpcs.com'"'"', '"'"'email:8165227655@tmomail.net'"'"'/g' /usr/local/etc/pi_garage_alert_config.py
 
 # Update GPIO pin numbers
 echo "Update GPIO pin numbers"
-sed -i-e 's/15/24/g' test.py
+sed -i-e 's/15/24/g' /usr/local/etc/pi_garage_alert_config.py
 
