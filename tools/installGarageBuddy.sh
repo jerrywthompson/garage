e2d6a8f774e39a43beea2ff49e87872db3ea3e53
jerrywthompson <jerrywthompson@comcast.net>
2018-11-02 10:52:26 -0500

    Update installGarageBuddy.sh

--- tools/installGarageBuddy.sh
+++ tools/installGarageBuddy.sh
@@ -2,7 +2,7 @@
 
 ### Installation steps:
 ### 1.  Clone Project from GitHub
-##  	  git clone https://github.com/jerrywthompson/garagebuddy.git && cd garagebuddy && sudo apt-get -y install expect
+##  	  git clone https://github.com/jerrywthompson/garagebuddy.git && cd garagebuddy && git checkout master && sudo apt-get -y install expect
 ### 2.  Update the config file located here:   
 	  /home/pi/garagebuddy/garagebuddy.config
 ### 3.  Run the install script from /home/pi/garagebuddy Note: script is inside of /tools but run from garagebuddy directory
