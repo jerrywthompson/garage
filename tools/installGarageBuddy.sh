3cb1fc1a64ae0c9d59e1595cbe966c44818b5eed
jerrywthompson <jerrywthompson@comcast.net>
2018-11-02 10:50:44 -0500

    Update installGarageBuddy.sh

--- tools/installGarageBuddy.sh
+++ tools/installGarageBuddy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 ### Installation steps:
-### Clone Project from GitHub
-##  	git clone https://github.com/jerrywthompson/garagebuddy.git && cd garagebuddy && sudo apt-get -y install expect
-### Update the config file located here:   /home/pi/garagebuddy/garagebuddy.config
-### Run the install script from /home/pi/garagebuddy Note: script is inside of /tools but run from garagebuddy directory
-##  	unbuffer bash tools/installGarageBuddy.sh 2>&1 | tee garageBuddyInstall.log
-#### NOTE:  Runtime logs are stored here:  tail -f /var/log/pi_garage_alert.log
+### 1.  Clone Project from GitHub
+##  	  git clone https://github.com/jerrywthompson/garagebuddy.git && cd garagebuddy && sudo apt-get -y install expect
+### 2.  Update the config file located here:   
+	  /home/pi/garagebuddy/garagebuddy.config
+### 3.  Run the install script from /home/pi/garagebuddy Note: script is inside of /tools but run from garagebuddy directory
+##  	  unbuffer bash tools/installGarageBuddy.sh 2>&1 | tee garageBuddyInstall.log
+### NOTE:  Install logs are store here:   /home/pi/garagebuddy/garageBuddyInstall.log
+### NOTE:  Runtime logs are stored here:  tail -f /var/log/pi_garage_alert.log
 
 
 echo "********** Script started *********"
